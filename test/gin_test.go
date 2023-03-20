75fcb1f8609cfc978cc01cfb5865a939ff39fb4f
liaoqinzhou_sz <liaoqinzhou@sz.baidu.com>
2023-03-20 17:50:31 +0800

    增加同service调用方式

--- test/gin_test.go
+++ test/gin_test.go
@@ -27,8 +27,28 @@ type TestParam struct {
 	Param string `validate:"required"`
 }
 
-func (s *TestParam) Exec() (any, error) {
-	return fmt.Sprintf("[%s]handler [%s]token [%s]param", s.str, s.Token, s.Param), nil
+func (p *TestParam) Exec() (any, error) {
+	return fmt.Sprintf("[%s]handler [%s]token [%s]param", p.str, p.Token, p.Param), nil
+}
+
+type Test2Param struct {
+	TestService
+	Token string `validate:"required"`
+	Param string `validate:"required"`
+}
+
+func (p *Test2Param) Exec() (any, error) {
+	return fmt.Sprintf("[%s]handler [%s]token [%s]param", p.str, p.Token, p.Param), nil
+}
+
+type Test3Param struct {
+	TestService
+	Token string `validate:"required"`
+	Param string `validate:"required"`
+}
+
+func (p *Test3Param) Exec() (any, error) {
+	return (&Test2Param{Token: p.Token, Param: p.Param, TestService: TestService{str: "test3"}}).Exec()
 }
 
 //启动http
@@ -38,9 +58,17 @@ func TestWeb(t *testing.T) {
 		home := g.Group("/test").Use()
 		{
 			//get请求
-			home.GET("", func(c *gin.Context) {
+			home.GET("/1", func(c *gin.Context) {
 				//query参数解析并执行
 				(&middleware.AbstractParser[*TestParam]{C: c}).Query("param", "token")
+			})
+			home.GET("/2/:param/:token", func(c *gin.Context) {
+				//query参数解析并执行
+				(&middleware.AbstractParser[*Test2Param]{C: c}).Path("param", "token")
+			})
+			home.GET("/3", func(c *gin.Context) {
+				//query参数解析并执行
+				(&middleware.AbstractParser[*Test3Param]{C: c}).Query("param", "token")
 			})
 		}
 	})
